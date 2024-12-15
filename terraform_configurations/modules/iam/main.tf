7db360c4edbde87d29a31a3de9f9824fe2710785
yifattih <yifattih@proton.me>
2024-12-15 22:30:45 +0000

    core: change sa name

--- terraform_configurations/modules/iam/main.tf
+++ terraform_configurations/modules/iam/main.tf
@@ -1,5 +1,5 @@
 resource "google_service_account" "cloud_run_sa" {
-  account_id   = "rmr-projection-shared-sa"
+  account_id   = "rmr-projection-sa"
   display_name = "Shared RMR Projection Project Service Account"
   create_ignore_already_exists = true
 }
