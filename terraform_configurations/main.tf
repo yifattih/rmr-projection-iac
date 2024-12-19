f81a478a0e5b0b5497f261997411b58ee090ad99
yifattih <yifattih@proton.me>
2024-12-19 20:32:57 +0000

    core: remove mapping

--- terraform_configurations/main.tf
+++ terraform_configurations/main.tf
@@ -1,28 +1,28 @@
 module "iam" {
   source     = "./modules/iam"
-  project_id = var.gcp["project_id"]
-  region     = var.gcp["region"]
+  project_id = var.project_id
+  region     = var.region
 }
 
 module "api_service" {
   source          = "./modules/cloud_run"
-  project_id      = var.gcp["project_id"]
-  region          = var.gcp["region"]
-  name            = var.api_service["name"]
-  revision_suffix = var.api_service["revision_suffix"]
-  environment     = var.api_service["environment"]
-  image_uri       = var.api_service["image_uri"]
-  container_port  = var.api_service["container_port"]
+  project_id      = var.project_id
+  region          = var.region
+  name            = var.api_service_name
+  revision_suffix = var.api_revision_suffix
+  environment     = var.api_environment
+  image_uri       = var.api_image_uri
+  container_port  = var.api_container_port
 }
 
 module "webapp_service" {
   source          = "./modules/cloud_run"
-  project_id      = var.gcp["project_id"]
-  region          = var.gcp["region"]
-  name            = var.client_service["name"]
-  revision_suffix = var.client_service["revision_suffix"]
-  environment     = var.client_service["environment"]
-  image_uri       = var.client_service["image_uri"]
-  container_port  = var.client_service["container_port"]
+  project_id      = var.project_id
+  region          = var.region
+  name            = var.client_service_name
+  revision_suffix = var.client_revision_suffix
+  environment     = var.client_environment
+  image_uri       = var.client_image_uri
+  container_port  = var.client_container_port
   env_vars        = { API_URL = module.api_service.url }
 }
--- terraform_configurations/variables.tf
+++ terraform_configurations/variables.tf
@@ -1,14 +1,62 @@
-variable "gcp" {
-  type        = map(string)
-  description = "Map with Google Cloud Platform project settings."
+# variable "gcp" {
+#   type        = map(string)
+#   description = "Map with Google Cloud Platform project settings."
+# }
+
+# variable "api_service" {
+#   type        = map(string)
+#   description = "Map with service settings"
+# }
+
+# variable "client_service" {
+#   type        = map(string)
+#   description = "Map with service settings"
+# }
+
+variable "project_id" {
+  type = string  
 }
 
-variable "api_service" {
-  type        = map(string)
-  description = "Map with service settings"
+variable "region" {
+  type = string
 }
 
-variable "client_service" {
-  type        = map(string)
-  description = "Map with service settings"
+variable "api_service_name" {
+  type = string
+}
+
+variable "api_revision_suffix" {
+  type = string
+}
+
+variable "api_environment" {
+  type = string
+}
+
+variable "api_image_uri" {
+  type = string
+}
+
+variable "api_container_port" {
+  type = number
+}
+
+variable "client_service_name" {
+  type = string
+}
+
+variable "client_revision_suffix" {
+  type = string
+}
+
+variable "client_environment" {
+  type = string
+}
+
+variable "client_image_uri" {
+  type = string
+}
+
+variable "client_container_port" {
+  type = number
 }
