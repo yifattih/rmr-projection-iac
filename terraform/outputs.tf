69d4ea0ad1e50bbb396a75b9c9de7ce90163a05d
yifattih <yifattih@proton.me>
2024-12-23 19:38:31 +0000

    core: fix outputs references

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,15 @@
-output "api_url" {
-  value = module.api_service.url
+output "api_service_blue_url" {
+  value = module.api_service_blue.url
 }
 
-output "webapp_url" {
-  value = module.webapp_service.url
+output "api_service_green_url" {
+  value = module.api_service_green.url
+}
+
+output "client_service_blue_url" {
+  value = module.client_service_blue.url
+}
+
+output "client_service_green_url" {
+  value = module.client_service_green.url
 }
